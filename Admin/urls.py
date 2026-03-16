de2984be7816247a66b726670d81b73ac41daa74
unknown <you@example.com>
2026-03-16 13:16:02 +0300

    modelda ozagrish boldi

--- Admin/urls.py
+++ Admin/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', iclude("main.urls") )
+    #path('', iclude("main.urls") )
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
 
